6299d913ff5d1f4eb16de6c2f7890ba5e008603d
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-06-22 23:56:17 -0700

    OpenCV camera sever and client test
    Uses local host, might not use this as I have to use it through ethernet

---
+++ TestingCode/videoClient.py
@@ -0,0 +1,30 @@
+import cv2 
+import socket
+import pickle
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
+
+server_ip = '127.0.0.1'
+server_port = 6666
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while cap.isOpened():
+
+    ret, img = cap.read()
+
+    cv2.imshow('Img Client', img)
+
+    ret, buffer = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+    x_as_bytes = pickle.dumps(buffer)
+
+    s.sendto((x_as_bytes), (server_ip, server_port))
+
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
+cap.release()
